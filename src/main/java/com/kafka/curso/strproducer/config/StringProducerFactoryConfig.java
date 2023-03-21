04a4c1e1537a69a27568d48dfacf4f5bd90a5515
Cristian Paes <cristian.ferreira@matera.com>
2023-03-21 15:11:21 -0300

    Criando topico e enviando mensagem

---
+++ src/main/java/com/kafka/curso/strproducer/config/StringProducerFactoryConfig.java
@@ -0,0 +1,40 @@
+package com.kafka.curso.strproducer.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import javax.websocket.OnClose;
+import java.util.HashMap;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Configuration
+public class StringProducerFactoryConfig {
+
+    private final KafkaProperties properties;
+
+    //Criando uma sereliazação para enviar as mensagens para o topico
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        var configs = new HashMap<String, Object>();
+
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configs);
+
+    }
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
---
+++ src/main/java/com/kafka/curso/strproducer/resources/StringProducerResources.java
@@ -0,0 +1,23 @@
+package com.kafka.curso.strproducer.resources;
+
+import com.kafka.curso.strproducer.services.StringProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/producer")
+public class StringProducerResources {
+
+    private final StringProducerService producerService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message) {
+        producerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+}
